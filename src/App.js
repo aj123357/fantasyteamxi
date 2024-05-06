91ab6b16390d3dbe787ad059fa9cd59f853eb525
Shilpzz <shilpi.dhiman@gmail.com>
2024-05-07 00:27:09 +0530

    style chnages continued

--- src/App.js
+++ src/App.js
@@ -22,18 +22,19 @@ const App = () => {
         <Login setLoggedIn={setLoggedIn} />
       ) : (
         <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {loggedIn && <Route path="/contact" element={<Contact />} />}
-          {loggedIn && <Route path="/bets" element={<Bets />} />}
-        </Routes>
-        <button
+          <button
           className="withdraw-btn"
           disabled={fetchWalletAmount() === 0}
           onClick={() => console.log("withdraw")}
         >
           Withdraw Amount
         </button>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {loggedIn && <Route path="/contact" element={<Contact />} />}
+          {loggedIn && <Route path="/bets" element={<Bets />} />}
+        </Routes>
+      
       </>
      
       )}
