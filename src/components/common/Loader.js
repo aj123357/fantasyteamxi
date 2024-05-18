cc5b906aa1162844540eac22fdf7d4febe82a13d
Shilpzz <shilpi.dhiman@gmail.com>
2024-05-18 18:42:57 +0530

    loader css changes

--- src/components/common/Loader.js
+++ src/components/common/Loader.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { CircularProgress } from "@mui/material";
 
 const Loader = () => {
-  return <CircularProgress color="secondary" />;
+  return <CircularProgress style={{'margin-left': '3px'}} color="secondary" size="0.8rem" />;
 };
 
 export default Loader;
